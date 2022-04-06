58d5e22d5ed5ef6e51f9b0ab861accc140668cfc
AllanGBOliveira <allebeira@gmail.com>
2022-04-06 14:48:10 -0300

    14fcbb3

---
+++ _nuxt/bd71cf5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{407:function(e,n,t){"use strict";t.r(n);var r={name:"Emoji",props:{code:{required:!0}}},o=t(39),component=Object(o.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
